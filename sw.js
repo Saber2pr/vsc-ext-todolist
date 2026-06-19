4d8b006169852e74e841125430c125597b0e9224
Saber2pr <Saber2pr@users.noreply.github.com>
2026-06-19 12:51:23 +0000

    deploy: Saber2pr/todolist-app@d102c90165c73edcea45afac7bfe099f41530e55

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-restricted-globals */
 /**
- * Todo List PWA service worker. 0.2.219 is replaced at build time.
+ * Todo List PWA service worker. 0.2.221 is replaced at build time.
  */
-const CACHE_NAME = 'todolist-0.2.219'
+const CACHE_NAME = 'todolist-0.2.221'
 
 const PRECACHE_URLS = [
   '/',
